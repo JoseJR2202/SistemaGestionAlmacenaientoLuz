94e3f82d4d17dd80c117836c19a4a56f15de0d52
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-11-16 13:37:48 -0400

    navbar-v2

--- frontend/src/component/navBar.js
+++ frontend/src/component/navBar.js
@@ -4,82 +4,45 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import { FaUserCircle } from 'react-icons/fa';
 import Investigation from '../pages/investigation/investigation';
 import Login from '../pages/user/login';
 import Home from '../pages/home';
 import Profile from '../pages/user/profile';
 import Meetings from '../pages/meetings/meetings';
 import Error from '../pages/error404';
-import { 
-    Navbar,
-    NavItem,
-    NavbarBrand,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
-    NavbarText,
-    NavbarToggler,
-    Collapse,
-    Nav
-  } from 'reactstrap';
+import {ReactComponent as ReactLogo} from '../img/logo.svg';
 
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const navBar = () => {
-  return (
+
+    return (
     <div>
-        <Navbar
-            color="light"
-            expand="md"
-            light
-        >
-        <NavbarBrand href="/">
-            Logo
-        </NavbarBrand>
-        <NavbarToggler onClick={function noRefCheck(){}} />
-        <Collapse navbar > 
-        <Nav
-            className="ms-auto"
-            navbar
-        >
-        <NavItem>
-            <NavLink href="/login">
-                Login
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href="/">
-                Home
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href="/reuniones">
-                reuniones
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href="/investigaciones">
-                investigaciones
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href="/perfil">
-                perfil
-            </NavLink>
-        </NavItem>
-      </Nav>
-    </Collapse>
-  </Navbar>
-    <Routes>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/" element={<Home />}/>
-      <Route path="/reuniones" element={<Meetings />}/>
-      <Route path="/investigaciones" element={<Investigation />}/>
-      <Route path="/perfil" element={<Profile />}/>
-      <Route path="*" element={<Error/>}/>
-    </Routes>
-  </div>
+        <Navbar bg="light" expand="lg" >
+            <Container>
+                <Navbar.Brand href="/"><ReactLogo/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/reuniones">Reuniones</Nav.Link>
+                    <Nav.Link href="/investigaciones">Investigaciones</Nav.Link>
+                    <Nav.Link href="/perfil">perfil <FaUserCircle size="25"/></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/reuniones" element={<Meetings />}/>
+            <Route path="/investigaciones" element={<Investigation />}/>
+            <Route path="/perfil" element={<Profile />}/>
+            <Route path="*" element={<Error/>}/>
+        </Routes>
+    </div>
   )
 }
 
