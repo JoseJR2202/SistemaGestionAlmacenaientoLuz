e5a34d3967f1b7cd31af7db39f582dc1eced09da
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-11-24 00:50:26 -0400

    table-v1
    componente de tabla dinamica con:
    -paginacion
    -seleccion
    -responsive

---
+++ frontend/src/component/table.js
@@ -0,0 +1,88 @@
+import React, {useMemo} from 'react'
+import { Table, ButtonToolbar, ButtonGroup, Button} from 'react-bootstrap';
+import { useTable, usePagination } from "react-table";
+
+const TableContend=({head, contend})=>{
+
+    const data= useMemo(()=>contend,[]);
+    const columns= useMemo(()=>head,[]);
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        page,
+        prepareRow,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        state: { pageIndex, pageSize },
+    } = useTable({ columns, data, initialState: { pageIndex: 0, pageSize:2}}, usePagination)
+
+    return(
+        <div>
+            <Table striped bordered hover responsive {...getTableProps()} >
+                <thead>
+                    {// Loop over the header rows
+                    headerGroups.map(headerGroup => (
+                        // Apply the header row props
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                        {// Loop over the headers in each row
+                        headerGroup.headers.map(column => (
+                            // Apply the header cell props
+                            <th {...column.getHeaderProps()}>
+                            {// Render the header
+                            column.render('Header')}
+                            </th>
+                        ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {// Loop over the table rows
+                    page.map((row, i) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps({
+                            onClick:()=>{console.log(row.original.id)}
+                            })}>
+                            {row.cells.map(cell => {
+                              return <td {...cell.getCellProps()} >{cell.render('Cell')}</td>
+                            })}
+                          </tr>
+                        )
+                      })}
+                </tbody>
+            </Table>
+            <ButtonToolbar aria-label="Toolbar with button groups">
+                <ButtonGroup className="me-2" aria-label="First group">
+                    <Button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                    </Button>
+                    <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                    </Button>
+                    <Button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                    </Button>
+                    <Button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                    </Button>
+                    <span>
+                    pagina{' '}
+                    <strong>
+                        {pageIndex + 1} de {pageOptions.length}
+                    </strong>{' '}
+                    </span>
+                </ButtonGroup>
+            </ButtonToolbar>
+        </div>
+        
+    )
+}
+
+export default TableContend;
--- frontend/src/pages/investigation/investigation.js
+++ frontend/src/pages/investigation/investigation.js
@@ -1,12 +1,51 @@
 import React from 'react'
 import Navbar from '../../component/navBar'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from '../../component/table'
 
 const investigation = () => {
   return (
-    <div>
-      <Navbar/>
-      esta es la vista de investigaciones
-    </div>
+    <Container fluid={true}>
+      <Row>
+        <Navbar/>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs="auto">
+          <h3>Buscador de Proyectos de Investigacion</h3>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+          <Table 
+          head={[{
+              Header:"Proyecto de investigacion",
+              accessor:'titulo'
+          }, {
+            Header:"escuela",
+            accessor:'extra'
+          }]}
+          contend={[{
+            id:1,
+            titulo:"proyecto de prueba 1",
+            extra:"escuela de prueba"
+          },{
+            id:7,
+            titulo:"proyecto de prueba 2",
+            extra:"escuela de prueba"
+          },{
+            id:3,
+            titulo:"proyecto de prueba 3",
+            extra:"escuela de prueba"
+          },{
+            id:4,
+            titulo:"proyecto de prueba 5",
+            extra:"escuela de prueba"
+          }]}
+          />
+      </Row>
+    </Container>
+   
   )
 }
 
